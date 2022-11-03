62cc7e454fc8b85cd973d75a986b9104fd6b34df
KirillAtrakhimovich <kirill.atrakhimovich@gmail.com>
2022-11-03 19:41:18 +0300

    NewsFilterView creating

--- steam/ViewControllers/NewsScreen/NewsFilterView.swift
+++ steam/ViewControllers/NewsScreen/NewsFilterView.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+class NewsFilterView: NiblessView {
+        
+    private(set) var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = UIColor.clear
+        tableView.register(NewsFilterTableCell.self, forCellReuseIdentifier: "NewsFilterTableCell")
+        tableView.separatorColor = .white
+        return tableView
+    }()
+    
+    private(set) var saveButton: UIButton = {
+        let saveButton = UIButton()
+        saveButton.backgroundColor = UIColor(named: "navBarColor")
+        saveButton.layer.cornerRadius = 5
+        saveButton.setTitle("Save", for: .normal)
+        return saveButton
+    }()
+    
+    func setup() {
+        setupTableView()
+        setupSaveButton()
+    }
+    
+    private func setupTableView() {
+        self.addSubview(tableView)
+        tableView.snp.makeConstraints { constraints in
+            constraints.top.equalToSuperview().offset(10)
+            constraints.leading.equalToSuperview().offset(5)
+            constraints.trailing.equalToSuperview().inset(-5)
+        }
+    }
+    
+    private func setupSaveButton() {
+        self.addSubview(saveButton)
+        saveButton.snp.makeConstraints { constraints in
+            constraints.top.equalTo(tableView.snp.bottom).offset(10)
+            constraints.leading.equalToSuperview().offset(25)
+            constraints.trailing.equalToSuperview().inset(25)
+            constraints.bottom.equalToSuperview().inset(10)
+        }
+    }
+    
+}
