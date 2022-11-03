09d2a2ed17d3233a553688185faec33dc8bae4bc
KirillAtrakhimovich <kirill.atrakhimovich@gmail.com>
2022-11-03 19:40:06 +0300

    create NewsFilterTabelCell and NewsFilterView

---
+++ steam/ViewControllers/NewsScreen/NewsFilterTableCell.swift
@@ -0,0 +1,8 @@
+//
+//  NewsFilterTableCell.swift
+//  steam
+//
+//  Created by Kirill Atrakhimovich on 3.11.22.
+//
+
+import Foundation
---
+++ steam/ViewControllers/NewsScreen/NewsFilterView.swift
@@ -0,0 +1,8 @@
+//
+//  NewsFilterView.swift
+//  steam
+//
+//  Created by Kirill Atrakhimovich on 3.11.22.
+//
+
+import Foundation
