af47c9cc8f2f34cd7b10a8ae6d54559a227d2944
KirillAtrakhimovich <kirill.atrakhimovich@gmail.com>
2022-11-04 19:36:23 +0300

    ftc logic

--- steam/ViewControllers/NewsScreen/NewsFilterTableCell.swift
+++ steam/ViewControllers/NewsScreen/NewsFilterTableCell.swift
@@ -8,8 +8,34 @@
 import Foundation
 import UIKit
 
-class NewsFilterTableCell:NiblessViewCell {
+class NewsFilterTableCell: NiblessViewCell {
     static let identifier = "NewsFilterTableCell"
     
+    let gameTitle: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.numberOfLines = 0
+        return label
+    }()
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: "NewsFilterTableCell")
+        setupView()
+        self.backgroundColor = .clear
+        
+    }
+    
+    private func setupView() {
+        self.addSubview(gameTitle)
+        gameTitle.snp.makeConstraints { constraints in
+            constraints.top.equalToSuperview().offset(10)
+            constraints.leading.equalToSuperview().offset(20)
+            constraints.bottom.equalToSuperview().offset(-10)
+            constraints.trailing.equalToSuperview().offset(-20)
+        }
+    }
+
+    func fillCell(game: LocalFavoriteGame) {
+        gameTitle.text = game.name
+    }
 }
