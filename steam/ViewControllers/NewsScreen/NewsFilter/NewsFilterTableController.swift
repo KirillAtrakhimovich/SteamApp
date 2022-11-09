8ef32d4234ed8e47b985ce87b2636fef78a730fd
KirillAtrakhimovich <kirill.atrakhimovich@gmail.com>
2022-11-09 20:30:19 +0300

    refactoring tableController

--- steam/ViewControllers/NewsScreen/NewsFilter/NewsFilterTableController.swift
+++ steam/ViewControllers/NewsScreen/NewsFilter/NewsFilterTableController.swift
@@ -2,30 +2,21 @@ import Foundation
 import UIKit
 
 final class NewsFilterTableController: NSObject, UITableViewDelegate, UITableViewDataSource {
-    private var model: FavGameListModel?
-    private let persist: PersistenceManager
-    init(persist: PersistenceManager) {
-        self.persist = persist
-        super.init()
+    private var games = [GameFilterModel]()
+    
+    func configure(_ games: [GameFilterModel]) {
+        self.games = games
     }
     
-//    private func createModel(games: [Favorite]) {
-//        let gameItems = games.map { LocalFavoriteGame(id: $0.id, isFavorite: true, name: $0.name, discount: $0.discount, isDiscount: $0.isDiscount, price: $0.price, isFree: $0.isFree, finalPrice: $0.finalPrice)}
-//
-//        model = FavGameListModel(games: gameItems, filteredGames: gameItems)
-//    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        createModel(games: persist.getFavoriteGames())
-        return model?.filteredGames.count ?? 0
+        games.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsFilterTableCell", for: indexPath) as? NewsFilterTableCell else {
             return UITableViewCell()
         }
-        guard let model = model else { return UITableViewCell() }
-        let item = model.filteredGames[indexPath.row]
+        let item = games[indexPath.row]
         cell.fillCell(game: item)
         cell.isUserInteractionEnabled = true
         cell.selectionStyle = .none
