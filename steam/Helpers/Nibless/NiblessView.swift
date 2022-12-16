047ec9aab4cd16d5b9dbbcc458968f035ec6e406
Kirill <kirill.atrakhimovich@gmail.com>
2022-12-16 22:22:07 +0300

    nibless review

--- steam/Helpers/Nibless/NiblessView.swift
+++ steam/Helpers/Nibless/NiblessView.swift
@@ -8,6 +8,12 @@ open class NiblessView: UIView {
     
     @available(*, unavailable, message: "Loading this view from a nib is unsupported in favor of initializer dependency injection.")
     public required init?(coder: NSCoder) {
-        fatalError("Loading this view from a nib is unsupported in favor of initializer dependency injection.")
+        fatalError(Constants.error)
+    }
+}
+
+extension NiblessView {
+    struct Constants {
+        static let error = "Loading this view from a nib is unsupported in favor of initializer dependency injection."
     }
 }
